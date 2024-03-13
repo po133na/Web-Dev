20cb6c9665757d9bddfcde8abb54fc0a797fc821
po133na <antrauxxxxx@mail.ru>
2024-03-14 00:07:58 +0500

    css done

--- lab6/albums/src/app/services/albums.service.ts
+++ lab6/albums/src/app/services/albums.service.ts
@@ -18,21 +18,17 @@ export class AlbumsService {
   getAlbum(id: number) {
     return this.httpClient.get<Album>(`${this.BASE_URL}/albums/${id}`);
   }
-
-  addAlbum(album: Album) {
-    return this.httpClient.post<Album>(`${this.BASE_URL}/albums`, album);
-  }
-
-  deleteAlbum(id: number) {
-    return this.httpClient.delete<Album>(`${this.BASE_URL}/albums/${id}`);
-  }
-
   updateAlbum(albumId: number, newTitle: string) {
     const body = { title: newTitle };
     console.log(newTitle);
     return this.httpClient.put<Album>(`${this.BASE_URL}/albums/${albumId}`, body);
   }
-
+  addAlbum(album: Album) {
+    return this.httpClient.post<Album>(`${this.BASE_URL}/albums`, album);
+  }
+  deleteAlbum(id: number) {
+    return this.httpClient.delete<Album>(`${this.BASE_URL}/albums/${id}`);
+  }
   getAlbumPhotos(id: number) {
     return this.httpClient.get<AlbumPhoto[]>(`${this.BASE_URL}/albums/${id}/photos`);
   }
